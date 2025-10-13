43186dcf1c1686f6a62789bd8500c5cbe8614f58
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2025-10-12 20:14:05 -0400

    header for common types and functions

---
+++ smath/smath_common.hpp
@@ -0,0 +1,14 @@
+#ifndef SMATH_COMMON
+#define SMATH_COMMON
+
+#include "vector_type.hpp"
+#include "quaternion_type.hpp"
+#include "matrix_type.hpp"
+#include "transform_type.hpp"
+
+#include "vector_function.hpp"
+#include "matrix_function_impl.hpp"
+#include "quaternion_function.hpp"
+#include "transform_function.hpp"
+
+#endif
